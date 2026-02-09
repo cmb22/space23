070f110bcb740061b52d156ffa3ab62d98c77516
chris <chris@MacBook-Pro-2.local>
2026-02-09 20:18:13 +0100

    deployment test

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -4,10 +4,19 @@ import * as schema from './schema';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const url = process.env.POSTGRES_URL
+if (!url) throw new Error("No DB url");
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error('POSTGRES_URL environment variable is not set');
-}
+const u = new URL(url);
+console.log("DB:", {
+  vercelEnv: process.env.VERCEL_ENV,
+  host: u.hostname,
+  port: u.port,
+  db: u.pathname,
+});
+// if (!process.env.POSTGRES_URL) {
+//   throw new Error('POSTGRES_URL environment variable is not set');
+// }
 
-export const client = postgres(process.env.POSTGRES_URL);
+export const client = postgres(url);
 export const db = drizzle(client, { schema });
